fd3bb719a05e536b8fc8aad1ca1c460c908903bf
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-03 13:14:46 +0200

    Enable WAL, foreign key constraints, other performance pragmas

--- src/lib/server/db/db.ts
+++ src/lib/server/db/db.ts
@@ -3,12 +3,24 @@ import { Database } from 'bun:sqlite';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
+const PRAGMAS = [
+	'journal_mode = WAL', // Enable WAL to improve performance
+	'synchronous = NORMAL', // Should already be enabled by WAL, increases performance
+	'cache_size = 8192', // Increase cache size to improve performance
+	'temp_store = MEMORY', // Store temporary tables in memory to improve performance
+	'foreign_keys = ON', // Enable foreign key constraints
+	'mmap_size = 268435456' // Enable memory mapped I/O, increases performance, 256MB
+];
+
 const initializeDatabase = () => {
 	if (env.DB_FILE_NAME === undefined)
 		throw new Error('.env file is missing required DB_FILE_NAME key!');
 
 	const sqlite = new Database(`data/${env.DB_FILE_NAME}`);
+	PRAGMAS.forEach((pragma) => sqlite.run(`PRAGMA ${pragma}`));
+
 	const db = drizzle({ client: sqlite, casing: 'camelCase' });
+
 	migrate(db, {
 		migrationsFolder: 'drizzle'
 	});
