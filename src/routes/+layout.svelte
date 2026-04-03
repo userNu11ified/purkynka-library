d35fad68c20d813a033d747f60d8bf13df7788cd
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-03 10:30:51 +0200

    Add base styling, Inter font loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import '$style/font.css';
+	import '$style/theme.css';
+	import '$style/base.css';
 
 	let { children } = $props();
 </script>
@@ -8,4 +11,17 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="app-mount">
+	{@render children()}
+</div>
+
+<style>
+	.app-mount {
+		width: 100dvw;
+		height: 100dvh;
+
+		padding: 16px;
+
+		overflow: hidden;
+	}
+</style>
