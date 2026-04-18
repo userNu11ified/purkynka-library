99df10a3d4e455ddca246e78cbdd933d98c92e30
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-18 18:30:47 +0200

    Make sum return 0 with an empty array

--- src/lib/shared/types/util.ts
+++ src/lib/shared/types/util.ts
@@ -1,4 +1,4 @@
 export type Nullable<T> = T | null;
 export type ValueOf<T> = T[keyof T];
 
-export const sum = (values: number[]) => values.reduce((prev, curr) => prev + curr);
+export const sum = (values: number[]) => values.reduce((prev, curr) => prev + curr, 0);
