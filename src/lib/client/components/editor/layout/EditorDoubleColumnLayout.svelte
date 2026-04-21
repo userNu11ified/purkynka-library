7aae7854ec346dbce1f3dc93773a2e929fb2a350
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-22 00:10:45 +0200

    Add Editor layouts

---
+++ src/lib/client/components/editor/layout/EditorDoubleColumnLayout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import EditorSingleColumnLayout from './EditorSingleColumnLayout.svelte';
+
+	let { left, right }: { left: Snippet; right: Snippet } = $props();
+</script>
+
+<div class="editor-double-column-layout">
+	<EditorSingleColumnLayout>{@render left()}</EditorSingleColumnLayout>
+	<EditorSingleColumnLayout>{@render right()}</EditorSingleColumnLayout>
+</div>
+
+<style>
+	.editor-double-column-layout {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 64px;
+	}
+</style>
---
+++ src/lib/client/components/editor/layout/EditorSingleColumnLayout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<div class="editor-single-column-layout flex-column">
+	{@render children()}
+</div>
+
+<style>
+	.editor-single-column-layout {
+		gap: 8px;
+	}
+</style>
