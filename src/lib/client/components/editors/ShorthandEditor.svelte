493d481c68251864e92680e22f0e89dc740a293a
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-22 02:55:38 +0200

    Create Lookup and Shorthand Editor

---
+++ src/lib/client/components/editors/ShorthandEditor.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { LibrarianData } from '$shared/types/loaded_data/librarian_data';
+	import { onMount } from 'svelte';
+	import { trapFocus } from '../attachments/focus_trap.svelte';
+	import Editor from '../editor/Editor.svelte';
+	import EditorAction from '../editor/EditorAction.svelte';
+	import { EditorFieldIntegerState } from '../editor/inputs/state/editor_field_integer_state.svelte';
+	import { EditorFieldStringState } from '../editor/inputs/state/editor_field_string_state.svelte';
+	import EditorSingleColumnLayout from '../editor/layout/EditorSingleColumnLayout.svelte';
+	import EditorIntegerInputLine from '../editor/premade_lines/EditorIntegerInputLine.svelte';
+	import EditorStringInputLine from '../editor/premade_lines/EditorStringInputLine.svelte';
+	import Modal from '../Modal.svelte';
+	import { EditorPageStates } from './editor_page_states.svelte';
+	import { Result } from '$shared/types/result';
+	import { clientLogger } from '$client/client_loggers';
+
+	const editorPageStates = EditorPageStates.context.get();
+	const shorthandEditorState = $derived(editorPageStates.shorthandEditorState!);
+
+	const librarianData = LibrarianData.context.get();
+	const editingData = $derived.by(() => {
+		if (shorthandEditorState.type === 'literatureType') return librarianData.literatureTypes;
+		else return librarianData.udc;
+	});
+	const editedValue = $derived(editingData.getValueByIdOrNull(shorthandEditorState.id)!);
+
+	const editorState = $state({
+		id: new EditorFieldIntegerState('ID', {
+			inputOptions: { disabled: true, width: '128px', textAlignment: 'center' },
+			required: true
+		}),
+		shortName: new EditorFieldStringState('Short Name', { required: true }),
+		longName: new EditorFieldStringState('Long Name', { required: true })
+	});
+
+	const hasErrors = $derived(
+		Object.values(editorState).some((v) => v.parsed.parseErrors.length !== 0)
+	);
+
+	const onCancelClick = () => history.back();
+	const onSaveClick = async () => {
+		const id = editorState.id.getParsedValue()!;
+		const newValue = {
+			shortName: editorState.shortName.getParsedValue()!,
+			longName: editorState.longName.getParsedValue()!
+		};
+
+		const patchResult = await editingData.patch({ ids: [id], newValue });
+		if (Result.isError(patchResult)) {
+			clientLogger.fatal('Failed to PATCH shorthand!', { error: patchResult.value });
+			throw new Error();
+		}
+
+		history.back();
+	};
+
+	onMount(() => {
+		editorState.id.setFromValue(shorthandEditorState.id);
+		editorState.shortName.setFromValue(editedValue.shortName);
+		editorState.longName.setFromValue(editedValue.longName);
+	});
+</script>
+
+<Modal onClickOutside={() => history.back()}>
+	<Editor {@attach trapFocus(1)}>
+		{#snippet title()}
+			Edit
+		{/snippet}
+
+		{#snippet fields()}
+			<EditorSingleColumnLayout>
+				<EditorIntegerInputLine state={editorState.id}></EditorIntegerInputLine>
+				<EditorStringInputLine state={editorState.shortName}></EditorStringInputLine>
+				<EditorStringInputLine state={editorState.longName}></EditorStringInputLine>
+			</EditorSingleColumnLayout>
+		{/snippet}
+
+		{#snippet actions()}
+			<EditorAction actionColor="error" onClick={onCancelClick}>Cancel</EditorAction>
+			<EditorAction actionColor="success" disabled={hasErrors} onClick={onSaveClick}
+				>Save</EditorAction
+			>
+		{/snippet}
+	</Editor>
+</Modal>
--- src/lib/client/components/editors/editor_page_states.svelte.ts
+++ src/lib/client/components/editors/editor_page_states.svelte.ts
@@ -9,6 +9,20 @@ export type BookEditorPageState =
 
 export type UDCEditorPageState = { type: 'new' } | { type: 'edit'; udcId: number };
 
+export type MergeEditorPageState = { values: [string, number][] };
+
+export type LookupEditorPageState = {
+	type:
+		| 'bookName'
+		| 'authorName'
+		| 'publisher'
+		| 'placeOfPublishing'
+		| 'obtainedFrom'
+		| 'discardReason';
+	id: number;
+};
+export type ShorthandEditorPageState = { type: 'literatureType' | 'udc'; id: number };
+
 export class EditorPageStates {
 	public static context = new Context<EditorPageStates>('editor-page-states');
 
@@ -18,11 +32,29 @@ export class EditorPageStates {
 	public udcEditorState: UDCEditorPageState | undefined;
 	public udcEditorActive: boolean;
 
+	public mergeEditorState: MergeEditorPageState | undefined;
+	public mergeEditorActive: boolean;
+
+	public lookupEditorState: LookupEditorPageState | undefined;
+	public lookupEditorActive: boolean;
+
+	public shorthandEditorState: ShorthandEditorPageState | undefined;
+	public shorthandEditorActive: boolean;
+
 	constructor() {
 		this.bookEditorState = $derived(page.state.bookEditorState);
 		this.bookEditorActive = $derived(this.bookEditorState !== undefined);
 
 		this.udcEditorState = $derived(page.state.udcEditorState);
 		this.udcEditorActive = $derived(this.udcEditorState !== undefined);
+
+		this.mergeEditorState = $derived(page.state.mergeEditorState);
+		this.mergeEditorActive = $derived(this.mergeEditorState !== undefined);
+
+		this.lookupEditorState = $derived(page.state.lookupEditorState);
+		this.lookupEditorActive = $derived(this.lookupEditorState !== undefined);
+
+		this.shorthandEditorState = $derived(page.state.shorthandEditorState);
+		this.shorthandEditorActive = $derived(this.shorthandEditorState !== undefined);
 	}
 }
