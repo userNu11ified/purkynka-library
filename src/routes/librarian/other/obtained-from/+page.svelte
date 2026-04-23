c2d4e1dc3effbaaa90b3d6065f929ffe3ccdd9b7
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-23 06:58:25 +0200

    Create obtained from table view

---
+++ src/routes/librarian/other/obtained-from/+page.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+	import { pushState } from '$app/navigation';
+	import { clientLogger } from '$client/client_loggers';
+	import { stringFilter } from '$client/collation/filters';
+	import { mergeEditorSubmitCallbacks } from '$client/components/editors/MergeEditor.svelte';
+	import {
+		numberSorter,
+		stringSorter
+	} from '$client/components/table_view/logic/table_view_sorters';
+	import TableViewSelectAction from '$client/components/table_view/selection/TableViewSelectAction.svelte';
+	import TableView from '$client/components/table_view/TableView.svelte';
+	import { LibrarianData } from '$shared/types/loaded_data/librarian_data';
+	import { Result } from '$shared/types/result';
+
+	let tableView: TableView<any, any> | undefined = $state();
+
+	const librarianData = LibrarianData.context.get();
+	const obtainedFrom = $derived(librarianData.obtainedFrom.getArray());
+	const books = $derived(librarianData.books.getArray());
+	const usedDiscardReasonIds = $derived(new Set(books.map((v) => v.obtainedFromId)));
+
+	const onEditClick = (obtainedFromId: number) => {
+		tableView?.clearSelection();
+		pushState('', { lookupEditorState: { type: 'obtainedFrom', id: obtainedFromId } });
+	};
+
+	const onDeleteClick = async (obtainedFromId: number) => {
+		tableView?.clearSelection();
+
+		const deleteResult = await librarianData.obtainedFrom.delete([obtainedFromId]);
+		if (Result.isError(deleteResult)) {
+			clientLogger.fatal('Failed to delete!', { error: deleteResult.value });
+			throw new Error();
+		}
+	};
+
+	const onMergeClick = (obtainedFromValues: [string, number][]) => {
+		tableView?.clearSelection();
+		pushState('', { mergeEditorState: { values: obtainedFromValues } });
+	};
+
+	mergeEditorSubmitCallbacks.registerCallback(async ([selectedIndex, mergedIndices]) => {
+		const updateTo = obtainedFrom[selectedIndex].id;
+		const toUpdate = mergedIndices
+			.filter((v) => v !== selectedIndex)
+			.map((index) => obtainedFrom[index].id);
+
+		const booksToUpdate = books
+			.filter((v) => v.obtainedFromId !== null && toUpdate.includes(v.obtainedFromId))
+			.map((v) => ({ ...v, obtainedFromId: updateTo }));
+
+		if (booksToUpdate.length !== 0) {
+			const results = await Promise.all(
+				booksToUpdate.map((v) => librarianData.books.patch({ ids: [v.id], newValue: v }))
+			);
+			const patchErrors = results.filter((v) => Result.isError(v));
+			if (patchErrors.length !== 0) {
+				clientLogger.fatal('Failed to merge into books!', {
+					errors: patchErrors.map((v) => v.value)
+				});
+				throw new Error();
+			}
+		}
+
+		const deleteResult = await librarianData.obtainedFrom.delete(toUpdate);
+		if (Result.isError(deleteResult)) {
+			clientLogger.fatal('Failed to delete after merge!', { error: deleteResult.value });
+			throw new Error();
+		}
+	});
+</script>
+
+<TableView
+	bind:this={tableView}
+	renderAfterResolved={librarianData.loaded}
+	items={obtainedFrom}
+	itemMapper={({ id, value }) => ({ id, value })}
+	itemCopier={({ id, value }) => [id, value]}
+	columns={[
+		{
+			columnName: 'ID',
+			columnAlignment: 'center',
+			defaultColumnSize: { type: 'fr', fractions: 1 },
+
+			columnRenderer: { type: 'text', textCreator: (v) => v.id },
+			columnSorter: (l, r) => numberSorter(l.id, r.id),
+			columnSearcher: {
+				type: 'jumper',
+				jumper: (mappedItems, q) => {
+					const queryAsNumber = parseInt(q.lowercaseQuery);
+					if (Number.isNaN(queryAsNumber)) return null;
+
+					const foundIndex = mappedItems.findIndex(([v]) => v.id === queryAsNumber);
+					return foundIndex === -1 ? null : foundIndex;
+				}
+			}
+		},
+		{
+			columnName: 'Obtained From',
+			defaultColumnSize: { type: 'fr', fractions: 1 },
+
+			columnRenderer: { type: 'text', textCreator: (v) => v.value },
+			columnSorter: (l, r) => stringSorter(l.value, r.value),
+			columnSearcher: { type: 'filter', filter: (v, q) => stringFilter(v.value, q) }
+		}
+	]}
+>
+	{#snippet singleSelectActions(selectedItem)}
+		<TableViewSelectAction iconType="edit" onClick={() => onEditClick(selectedItem[0].id)}>
+			Edit
+		</TableViewSelectAction>
+		<TableViewSelectAction
+			iconType="trash-can"
+			color="error"
+			disabled={usedDiscardReasonIds.has(selectedItem[0].id)}
+			onClick={() => onDeleteClick(selectedItem[0].id)}
+		>
+			{#if usedDiscardReasonIds.has(selectedItem[0].id)}
+				This item is used somewhere!
+			{:else}
+				Delete
+			{/if}
+		</TableViewSelectAction>
+	{/snippet}
+
+	{#snippet multiSelectActions(selectedItems)}
+		<TableViewSelectAction
+			iconType="merge"
+			onClick={() => onMergeClick(selectedItems.map(([v, i]) => [v.value, i]))}
+		>
+			Merge
+		</TableViewSelectAction>
+	{/snippet}
+</TableView>
