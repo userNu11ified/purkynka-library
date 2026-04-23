620cb126927d475c0deee48201a5f6748b82f4ff
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-04-23 07:23:03 +0200

    Create udc table view

---
+++ src/routes/librarian/other/udc/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { pushState } from '$app/navigation';
+	import { clientLogger } from '$client/client_loggers';
+	import { stringFilter } from '$client/collation/filters';
+	import {
+		numberSorter,
+		stringSorter
+	} from '$client/components/table_view/logic/table_view_sorters';
+	import TableViewSelectAction from '$client/components/table_view/selection/TableViewSelectAction.svelte';
+	import TableView from '$client/components/table_view/TableView.svelte';
+	import { LibrarianData } from '$shared/types/loaded_data/librarian_data';
+	import { Result } from '$shared/types/result';
+
+	let tableView: TableView<any, any> | undefined = $state();
+
+	const librarianData = LibrarianData.context.get();
+	const udc = $derived(librarianData.udc.getArray());
+	const books = $derived(librarianData.books.getArray());
+	const usedLiteratureTypeIds = $derived(new Set(books.map((v) => v.udcId)));
+
+	const onEditClick = (udcId: number) => {
+		tableView?.clearSelection();
+		pushState('', {
+			shorthandEditorState: { type: 'udc', id: udcId }
+		});
+	};
+
+	const onDeleteClick = async (udcId: number) => {
+		tableView?.clearSelection();
+
+		const deleteResult = await librarianData.udc.delete([udcId]);
+		if (Result.isError(deleteResult)) {
+			clientLogger.fatal('Failed to delete!', { error: deleteResult.value });
+			throw new Error();
+		}
+	};
+</script>
+
+<TableView
+	renderAfterResolved={librarianData.loaded}
+	items={udc}
+	itemMapper={(v) => v}
+	itemCopier={({ id, shortName, longName }) => [id, shortName, longName]}
+	columns={[
+		{
+			columnName: 'ID',
+			columnAlignment: 'center',
+			defaultColumnSize: { type: 'fr', fractions: 1 },
+
+			columnRenderer: { type: 'text', textCreator: (v) => v.id },
+			columnSorter: (l, r) => numberSorter(l.id, r.id),
+			columnSearcher: {
+				type: 'jumper',
+				jumper: (mappedItems, q) => {
+					const queryAsNumber = parseInt(q.lowercaseQuery);
+					if (Number.isNaN(queryAsNumber)) return null;
+
+					const foundIndex = mappedItems.findIndex(([v]) => v.id === queryAsNumber);
+					return foundIndex === -1 ? null : foundIndex;
+				}
+			}
+		},
+		{
+			columnName: 'Short Name',
+			columnAlignment: 'center',
+			defaultColumnSize: { type: 'fr', fractions: 1 },
+
+			columnRenderer: { type: 'text', textCreator: (v) => v.shortName },
+			columnSorter: (l, r) => stringSorter(l.shortName, r.shortName),
+			columnSearcher: { type: 'filter', filter: (v, q) => stringFilter(v.shortName, q) }
+		},
+		{
+			columnName: 'Long Name',
+			defaultColumnSize: { type: 'fr', fractions: 1 },
+
+			columnRenderer: { type: 'text', textCreator: (v) => v.longName },
+			columnSorter: (l, r) => stringSorter(l.longName, r.longName),
+			columnSearcher: { type: 'filter', filter: (v, q) => stringFilter(v.longName, q) }
+		}
+	]}
+>
+	{#snippet singleSelectActions(selectedItem)}
+		<TableViewSelectAction iconType="edit" onClick={() => onEditClick(selectedItem[0].id)}
+			>Edit</TableViewSelectAction
+		>
+
+		<TableViewSelectAction
+			iconType="trash-can"
+			color="error"
+			disabled={usedLiteratureTypeIds.has(selectedItem[0].id)}
+			onClick={() => onDeleteClick(selectedItem[0].id)}
+		>
+			{#if usedLiteratureTypeIds.has(selectedItem[0].id)}
+				This item is used somewhere!
+			{:else}
+				Delete
+			{/if}
+		</TableViewSelectAction>
+	{/snippet}
+</TableView>
