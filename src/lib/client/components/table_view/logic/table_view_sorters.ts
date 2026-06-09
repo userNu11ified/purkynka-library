5b40372401decfa14c94adacae8b282cb3f9f6a6
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2026-06-09 02:20:11 +0200

    Make dateSorter work with Nullable<Date>

--- src/lib/client/components/table_view/logic/table_view_sorters.ts
+++ src/lib/client/components/table_view/logic/table_view_sorters.ts
@@ -1,5 +1,7 @@
+import type { Nullable } from '$shared/types/util';
+
 export const numberSorter = (left: number, right: number) => left - right;
 export const booleanSorter = (left: boolean, right: boolean) => numberSorter(+left, +right);
 export const stringSorter = (left: string, right: string) => left.localeCompare(right, 'cs');
-export const dateSorter = (left: Date, right: Date) =>
-	numberSorter(left.getTime(), right.getTime());
+export const dateSorter = (left: Nullable<Date>, right: Nullable<Date>) =>
+	numberSorter(left?.getTime() ?? 0, right?.getTime() ?? 0);
